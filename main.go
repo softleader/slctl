0bb14da9dc3697437cdb4a60a281fdfd8ca638bd
matt <methodho@gmail.com>
2018-11-16 16:57:45 +0800

    a start

--- main.go
+++ main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"github.com/softleader/slctl/pkg/environment"
+	"github.com/spf13/cobra"
+	"html/template"
+	"os"
+)
+
+var (
+	settings environment.EnvSettings
+)
+
+const (
+	Name        = "slctl"
+	globalUsage = `{{.}} controls SoftLeader services.
+
+To begin working with {{.}}, run the '{{.}} init' command:
+
+	$ {{.}} init
+
+It will set up any necessary local configuration.
+
+Common actions from this point include:
+
+Environment:
+  $SL_HOME           set an alternative location for {{.}} files.By default, these are stored in ~/.sl
+`
+)
 
 func main() {
-	fmt.Println("Hello SoftLeader Control")
+	cmd := newRootCmd(os.Args[1:])
+	if err := cmd.Execute(); err != nil {
+		switch e := err.(type) {
+		case pluginError:
+			os.Exit(e.code)
+		default:
+			os.Exit(1)
+		}
+	}
+}
+
+func usage() string {
+	var buf bytes.Buffer
+	t := template.Must(template.New("").Parse(globalUsage))
+	err := t.Execute(&buf, Name)
+	if err != nil {
+		panic(err)
+	}
+	return buf.String()
+}
+
+func newRootCmd(args []string) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:          Name,
+		Short:        Name + " controls SoftLeader services.",
+		Long:         usage(),
+		SilenceUsage: true,
+	}
+	flags := cmd.PersistentFlags()
+
+	settings.AddFlags(flags)
+
+	out := cmd.OutOrStdout()
+
+	cmd.AddCommand()
+
+	flags.Parse(args)
+
+	// set defaults from environment
+	settings.Init(flags)
+
+	// Find and add plugins
+	loadPlugins(cmd, out)
+
+	return cmd
 }
---
+++ pkg/environment/environment.go
@@ -0,0 +1,54 @@
+package environment
+
+import (
+	"github.com/softleader/slctl/pkg/homedir"
+	"github.com/softleader/slctl/pkg/slpath"
+	"github.com/spf13/pflag"
+	"os"
+	"path/filepath"
+)
+
+var DefaultHome = filepath.Join(homedir.HomeDir(), ".sl")
+
+type EnvSettings struct {
+	Home  slpath.Home
+	Debug bool
+}
+
+// AddFlags binds flags to the given flagset.
+func (s *EnvSettings) AddFlags(fs *pflag.FlagSet) {
+	fs.StringVar((*string)(&s.Home), "home", DefaultHome, "location of your config. Overrides $SL_HOME")
+	fs.BoolVar(&s.Debug, "debug", false, "enable verbose output")
+}
+
+// Init sets values from the environment.
+func (s *EnvSettings) Init(fs *pflag.FlagSet) {
+	for name, envar := range envMap {
+		setFlagFromEnv(name, envar, fs)
+	}
+}
+
+// envMap maps flag names to envvars
+var envMap = map[string]string{
+	"debug": "SL_DEBUG",
+	"home":  "SL_HOME",
+}
+
+func (s EnvSettings) PluginDirs() string {
+	if d, ok := os.LookupEnv("SL_PLUGIN"); ok {
+		return d
+	}
+	return s.Home.Plugins()
+}
+
+// setFlagFromEnv looks up and sets a flag if the corresponding environment variable changed.
+// if the flag with the corresponding name was set during fs.Parse(), then the environment
+// variable is ignored.
+func setFlagFromEnv(name, envar string, fs *pflag.FlagSet) {
+	if fs.Changed(name) {
+		return
+	}
+	if v, ok := os.LookupEnv(envar); ok {
+		fs.Set(name, v)
+	}
+}
