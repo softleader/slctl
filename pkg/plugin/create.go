a17b4cf8c870db46a7eb68d4233a6481e73c3dd8
matt <methodho@gmail.com>
2018-11-20 17:01:07 +0800

    update

--- pkg/plugin/create.go
+++ pkg/plugin/create.go
@@ -48,8 +48,10 @@ func main() {
 const defaultMakefile = `SL_HOME ?= $(shell slctl home)
 SL_PLUGIN_DIR ?= $(SL_HOME)/plugins/{{.Name}}/
 HAS_GLIDE := $(shell command -v glide;)
-VERSION := ""
+VERSION := $(shell sed -n -e 's/version:[ "]*\([^"]*\).*/\1/p' plugin.yaml)
+DIST := $(CURDIR)/_dist
 BUILD := $(CURDIR)/_build
+LDFLAGS := "-X main.version=${VERSION}"
 BINARY := {{.Name}}
 
 .PHONY: install
@@ -68,6 +70,19 @@ build: clean bootstrap
 	cp plugin.yaml $(BUILD)
 	go build -o $(BUILD)/$(BINARY)
 
+.PHONY: dist
+dist:
+	go get -u github.com/inconshreveable/mousetrap
+	mkdir -p $(BUILD)
+	mkdir -p $(DIST)
+	sed -E 's/(version: )"(.+)"/\1"$(VERSION)"/g' plugin.yaml > $(BUILD)/plugin.yaml
+	GOOS=linux GOARCH=amd64 go build -o $(BUILD)/$(BINARY) -ldflags $(LDFLAGS) -a -tags netgo
+	tar -C $(BUILD) -zcvf $(DIST)/$(BINARY)-linux-$(VERSION).tgz $(BINARY) plugin.yaml
+	GOOS=darwin GOARCH=amd64 go build -o $(BUILD)/$(BINARY) -ldflags $(LDFLAGS) -a -tags netgo
+	tar -C $(BUILD) -zcvf $(DIST)/$(BINARY)-macos-$(VERSION).tgz $(BINARY) plugin.yaml
+	GOOS=windows GOARCH=amd64 go build -o $(BUILD)/$(BINARY).exe -ldflags $(LDFLAGS) -a -tags netgo
+	tar -C $(BUILD) -llzcvf $(DIST)/$(BINARY)-windows-$(VERSION).tgz $(BINARY).exe plugin.yaml
+
 .PHONY: bootstrap
 bootstrap:
 ifndef HAS_GLIDE
@@ -76,7 +91,7 @@ endif
 ifeq (,$(wildcard ./glide.yaml))
 	glide init --non-interactive
 endif
-	glide install --strip-vendor
+	glide install --strip-vendor	
 
 .PHONY: clean
 clean:
