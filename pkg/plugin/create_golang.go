dca9a8634b5a8dfa60774301df3f10828746015e
matt <methodho@gmail.com>
2018-12-13 13:40:54 +0800

    update template

--- pkg/plugin/create_golang.go
+++ pkg/plugin/create_golang.go
@@ -43,10 +43,6 @@ func main() {
 	f.BoolVarP(&c.verbose, "verbose", "v", c.verbose, "enable verbose output, Overrides $SL_VERBOSE")
 	f.StringVar(&c.token, "token", "$SL_TOKEN", "github access token. Overrides $SL_TOKEN")
 
-	cmd.AddCommand(
-		newVersionCmd(c.out),
-	)
-
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
@@ -74,23 +70,10 @@ import (
 )
 
 const (
-	versionHelp = "print {{.Name}} version."
 	unreleased  = "unreleased"
 )
 
 var version string
-
-func newVersionCmd(out io.Writer) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "version",
-		Short: versionHelp,
-		Long:  versionHelp,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Fprintln(out, ver())
-		},
-	}
-	return cmd
-}
 
 func ver() string {
 	if v := strings.TrimSpace(version); v != "" {
