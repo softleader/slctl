b13a70149aefc79f50beefd5b3f2c9a4930e61ea
Sam.Wang <w32191@gmail.com>
2026-01-17 16:35:19 +0800

    refactor(init): Remove username/password flags and update usage

--- cmd/slctl/init.go
+++ cmd/slctl/init.go
@@ -18,38 +18,26 @@ import (
 )
 
 const (
-	initDesc = `This command grants Github access token and sets up local configuration in $SL_HOME (default ~/.config/slctl/).
+	initDesc = `This command authorizes slctl with GitHub and sets up local configuration in $SL_HOME (default ~/.config/slctl/).
 
-執行 'slctl init' 透過互動式的問答產生並儲存 GitHub Personal Access Token (https://github.com/settings/tokens)
-也可以傳入 '--username', '--password' 及 '--yes' 來整合非互動式的情境 (e.g. DevOps pipeline):
+To authenticate, slctl will use the GitHub Device Flow. You will be prompted to enter a code in your browser.
+
+Alternatively, you can provide a Personal Access Token (PAT) directly using the '--token' flag.
+The PAT must have the required scopes. To see them, run 'slctl init scopes'.
 
 	$ slctl init
-	$ slctl init -u GITHUB_USERNAME -p GITHUB_PASSWORD -y
-
-使用 '--force' 在發現有重複的 Token 時, 會強制刪除並產生一個全新的 Access Token
-
-	$ slctl init -f
-
-若你想自己維護 Access Token (請務必確保有足夠的權限), 可以使用 '--token' 讓 slctl 驗證後直接儲存起來
-執行 'scopes'' 可以列出所有 slctl 需要的 Access Token 權限
-
 	$ slctl init --token GITHUB_TOKEN
-	$ slctl init scopes
 
-使用 '--offline' 則 slctl 不會跟 GitHub API 有任何互動, 只會配置 $SL_HOME 環境目錄.
-
-同時使用 '--offline' 及 '--token' 可跳過 Token 驗證直接儲存起來 (e.g. 沒網路環境下)
+Using '--offline' skips all GitHub API interactions and only sets up local directories.
 `
 	askForPublicizeOrg = `Do you want to publicize the membership in SoftLeader?`
 )
 
 type initCmd struct {
-	home     paths.Home
-	username string
-	password string
-	token    string
-	force    bool
-	yes      bool
+	home  paths.Home
+	token string
+	force bool
+	yes   bool
 }
 
 func newInitCmd() *cobra.Command {
@@ -69,8 +57,6 @@ func newInitCmd() *cobra.Command {
 	f := cmd.Flags()
 	f.BoolVarP(&c.force, "force", "f", false, "force to re-generate a new one if token already exists")
 	f.StringVar(&c.token, "token", "", "github access token")
-	f.StringVarP(&c.username, "username", "u", "", "github username")
-	f.StringVarP(&c.password, "password", "p", "", "github password")
 	f.BoolVarP(&c.yes, "yes", "y", false, "automatic 'yes' to prompts. Assume 'yes' as answer to all prompts and run non-interactively")
 
 	cmd.AddCommand(
